c53867cea7170c9aff50ad1451ba47605ff2b917
Sushant Goel <g.sushant.viet.2009@gmail.com>
2015-10-03 19:36:37 +0530

    Add contents in program.cs

--- module3_language_drivers/CSharpStart/Program.cs
+++ module3_language_drivers/CSharpStart/Program.cs
@@ -1,8 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using MongoDB.Driver;
+using MongoDB.Driver.Builders;
+using MongoDB.Bson;
 
 namespace MongoSample
 {
@@ -10,6 +8,44 @@ namespace MongoSample
     {
         static void Main(string[] args)
         {
+            MongoClient client = new MongoClient("mongodb://127.0.0.1:27017/test");
+#pragma warning disable CS0618 // Type or member is obsolete
+            MongoServer server = client.GetServer();
+#pragma warning restore CS0618 // Type or member is obsolete
+
+            MongoDatabase database = server.GetDatabase("test");
+            MongoCollection<BsonDocument> bankdata = database.GetCollection<BsonDocument>("bank_data");
+
+            BsonDocument person = new BsonDocument
+            {
+                {"first_name","Steven" },
+                { "last_name","Edouard"},
+                {"accounts", new BsonArray
+                {
+                    new BsonDocument
+                    {
+                        {"account_balance",50000000 },
+                        {"account_type","Investment" },
+                        {"currency","USD" }
+                    }
+                } }
+            };
+
+            bankdata.Insert(person);
+            System.Console.WriteLine(person["_id"]);
+
+            person["accounts"][0]["account_balanace"] = person["accounts"][0]["account_balance"].AsInt32 + 100000;
+
+            bankdata.Save(person);
+            System.Console.WriteLine("Successfully updated 1 document");
+
+            BsonDocument newPerson = bankdata.FindOneById(person["_id"]);
+
+            System.Console.WriteLine(newPerson["accounts"][0]["account_balance"].AsInt32);
+
+            var query = Query.EQ("_id", newPerson["_id"]);
+            WriteConcernResult result = bankdata.Remove(query);
+            System.Console.WriteLine("number of documents removed " + result.DocumentsAffected);
         }
     }
 }
